0b1b91a6658178a862543c3a27291ed9f3327155
David Saenz Tagarro <david.saenz.tagarro@gmail.com>
2014-12-01 15:21:59 +0100

    Updated travis script

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141129235813) do
+ActiveRecord::Schema.define(version: 20141201081918) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "event_types", force: true do |t|
+    t.string "title"
+  end
+
+  create_table "events", force: true do |t|
     t.string "title"
   end
 
